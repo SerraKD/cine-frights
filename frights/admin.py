992254b5a258387537cfa270cfb9ba12a61fa8cc
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-07-23 19:17:57 +0000

    Register Comment model in admin.py

--- frights/admin.py
+++ frights/admin.py
@@ -1,6 +1,7 @@
 '''Imports'''
 from django.contrib import admin
-from frights.models import MoviePost
+from frights.models import MoviePost, Comment
 
 # Register your models here.
 admin.site.register(MoviePost)
+admin.site.register(Comment)
