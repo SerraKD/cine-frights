a98e96c0d5f4e52429f5b92c1b12a16514920373
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-07-24 08:32:15 +0000

    Install and wire up django-summernote

--- cinefrights/urls.py
+++ cinefrights/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from frights.views import cine_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('frights/', cine_blog, name='frights'),
 ]
