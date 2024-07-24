489f12da65ec25f738877eeecce70172ce8ae5a6
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-07-24 08:46:53 +0000

    Register summernote movie posts to admin.py

--- frights/admin.py
+++ frights/admin.py
@@ -1,7 +1,16 @@
 '''Imports'''
 from django.contrib import admin
 from frights.models import MoviePost, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(MoviePost)
+class MoviePostAdmin(SummernoteModelAdmin):
+    '''Registers Movie Posts to admin'''
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('summary',)
 
 # Register your models here.
-admin.site.register(MoviePost)
 admin.site.register(Comment)
