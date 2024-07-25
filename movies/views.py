7c6f4fa9a9c68d2377bba26d09e0b5597e4fb5ce
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-07-25 10:41:19 +0000

    move  views.py code from frights app to movies app, change template  path to movies

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Movie, Comment
 
 # Create your views here.
+class HomePageView(generic.ListView):
+    queryset = Movie.objects.filter(status=1)
+    template_name = "movies/index.html"
+    paginate_by = 4
