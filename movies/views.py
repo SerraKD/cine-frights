4893bb836342bb16ed322fd270fca24aa8527ebb
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-17 18:18:00 +0000

    Create a movie detail view in views.py

--- movies/views.py
+++ movies/views.py
@@ -1,10 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
 from .models import Movie, Comment
 
 # Create your views here.
 class HomePageView(generic.ListView):
+    ''' Method to display all movie posts'''
     queryset = Movie.objects.filter(status=1)
     template_name = "movies/index.html"
     paginate_by = 4
+
+def MovieDetailView(request, slug):
+    ''' to display selected movie post details'''
+    queryset = Movie.objects.filter(status=1)
+    movie = get_object_or_404(queryset, slug=slug)
+
+    return render(
+    request,
+    "movies/movie_detail.html",
+    {
+        "movie": movie,
+    },
+)
+
+    
+
+    
