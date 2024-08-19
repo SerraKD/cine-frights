3fa9467869285f9602b177e781a8d60208274f5c
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-19 15:05:18 +0000

    add import for homepageview in views.py

--- films/views.py
+++ films/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from movies.models import Movie, Comment
-from movies.views import MovieDetailView
+from movies.views import HomePageView, MovieDetailView
 
 
 # Create your views here.
