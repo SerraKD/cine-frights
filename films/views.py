8bdf946d9999fb6a6ae28e7cc7e331a5a5ffb61a
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-19 15:31:57 +0000

    create views for films in views.py

--- films/views.py
+++ films/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from movies.models import Movie, Comment
-from movies.views import HomePageView, MovieDetailView
 
 
 # Create your views here.
+class FilmsPageView(generic.ListView):
+    ''' Method to movie post corousel'''
+    queryset = Movie.objects.filter(status=1)
+    template_name = "films/films.html"
+
+def MovieDetailView(request, slug):
+    ''' to display selected movie post details'''
+    queryset = Movie.objects.filter(status=1)
+    movie = get_object_or_404(queryset, slug=slug)
+
+    return render(
+    request,
+    "movies/movie_detail.html",
+    {
+        "movie": movie,
+    },
+)
