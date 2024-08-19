4e286022b3d28064577a5f8118dbfce8588b4c7d
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-19 14:46:29 +0000

    Delete movie model in admin.py

--- films/admin.py
+++ films/admin.py
@@ -3,17 +3,4 @@ from movies.models import Movie, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-
-@admin.register(Movie)
-class MoviePostAdmin(SummernoteModelAdmin):
-    '''Registers Movie Posts to admin'''
-    list_display = ('title', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('summary',)
-    def __str__(self):
-        return self.title
-
-# Register your models here.
 admin.site.register(Comment)
