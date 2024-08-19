4fb7c32735a5c5cd373c29214dc96107c08c63c3
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-19 15:32:52 +0000

    change url paths to film page view in urls.py

--- films/urls.py
+++ films/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePageView.as_view(), name='films'),
+    path('', views.FilmsPageView.as_view(), name='films'),
     path('<slug:slug>/', views.MovieDetailView, name='movie_detail'),
 ]
