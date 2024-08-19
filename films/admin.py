5381f1103061dfadcd0f83aa9d889fc80a6f88c6
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-19 14:27:43 +0000

    Import summernote model admin ad add fields in admin.py

--- films/admin.py
+++ films/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Movie, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+@admin.register(Movie)
+class MoviePostAdmin(SummernoteModelAdmin):
+    '''Registers Movie Posts to admin'''
+    list_display = ('title', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('summary',)
+    def __str__(self):
+        return self.title
+
+# Register your models here.
+admin.site.register(Comment)
