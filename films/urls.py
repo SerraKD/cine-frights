c18194cccc35d2e57912ab2816fcd50534de5a31
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-19 15:11:45 +0000

    Fix the path for homepageview, add movie detail view in urls.py

--- films/urls.py
+++ films/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.HomePageView, name='films'),
+    path('', views.HomePageView.as_view(), name='films'),
+    path('<slug:slug>/', views.MovieDetailView, name='movie_detail'),
 ]
