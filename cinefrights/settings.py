6175fa43b18b299272b9b3ae68c572612fdc1c8e
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-20 19:00:19 +0000

    Set debug False

--- cinefrights/settings.py
+++ cinefrights/settings.py
@@ -33,7 +33,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["8000-serrakd-cinefrights-0j8finw9vzr.ws.codeinstitute-ide.net", ".herokuapp.com",
  "https://cine-frights-ed31240ece67.herokuapp.com/", "https://*.gitpod.io"]
