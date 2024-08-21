611d9f620fc7514bc3843ad8e029ab60180a234e
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-21 08:48:33 +0000

    Add crispy forms to settings.py

--- cinefrights/settings.py
+++ cinefrights/settings.py
@@ -52,6 +52,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'cloudinary_storage',
     'django_summernote',
     'cloudinary',
