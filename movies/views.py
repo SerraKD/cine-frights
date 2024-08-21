e6e388a144e47b519c08417fdf08f8b37d2e5c6e
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-21 11:23:37 +0000

    Add movie comments to moviedetailview in views.py

--- movies/views.py
+++ movies/views.py
@@ -13,12 +13,17 @@ def MovieDetailView(request, slug):
     ''' to display selected movie post details'''
     queryset = Movie.objects.filter(status=1)
     movie = get_object_or_404(queryset, slug=slug)
+    comments = Comment.objects.all().order_by("-created_on")
+    comment_count = Comment.objects.filter(approved=True).count()
+    movie.movie_comments.filter(approved=True).count()
 
     return render(
     request,
     "movies/movie_detail.html",
     {
         "movie": movie,
+        "comments": comments,
+        "comment_count": comment_count,
     },
 )
 
