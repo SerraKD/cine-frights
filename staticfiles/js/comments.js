60061d75101a2d5d4396e9074698310604bfc1fe
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-22 10:21:01 +0000

    Create comments.js, add function for edit comment button

---
+++ staticfiles/js/comments.js
@@ -0,0 +1,16 @@
+/*  edit comment buttons */
+const editButtons = document.getElementsByClassName("btn-edit");
+const commentText = document.getElementById("id_body");
+const commentForm = document.getElementById("commentForm");
+const submitButton = document.getElementById("submitButton");
+
+/* edit comment func */
+for (let button of editButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        let commentContent = document.getElementById(`comment${commentId}`).innerText;
+        commentText.value = commentContent;
+        submitButton.innerText = "Update";
+        commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
+}
