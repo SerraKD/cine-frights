a27d631db2fee769157800fe1b4636764bd61356
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-22 12:30:15 +0000

    Add urls pattern for comment delete in urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('<slug:slug>/', views.MovieDetailView, name='movie_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
