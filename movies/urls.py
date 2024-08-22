a39e687f3e84219e5fc4eb84c6854fa11b396276
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-22 09:23:33 +0000

    create a url path for editing comment view in urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('<slug:slug>/', views.MovieDetailView, name='movie_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
