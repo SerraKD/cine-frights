6250b8a0e63cad76ac8e38f412dd63311dcbd616
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-24 13:20:35 +0000

    run collect static

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -1,6 +1,6 @@
 /*  edit and delete comment buttons */
 const editButtons = document.getElementsByClassName("btn-edit");
-const commentText = document.getElementById("id_body");
+const commentText = document.getElementById("id_content");
 const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
