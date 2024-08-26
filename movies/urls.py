ce8e3c4510645a557cfe6fdced7e806be5674815
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-26 10:22:13 +0000

    Create a url pattern for recommending movie in urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
+    path('recommend/', views.recommend_view, name='recommend'),
     path('<slug:slug>/', views.MovieDetailView, name='movie_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
