80bdbc0314b3e8c0bc30e8d5ccc22a366a30eb77
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-26 19:26:55 +0000

    Create views for newsletter, add form imports, delete unused movie detail in views.py

--- films/views.py
+++ films/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
+from django.contrib import messages
 from movies.models import Movie, Comment
+from .models import NewsLetter
+from .forms import NewsLetterForm
 
 
-# Create your views here.
 class FilmsPageView(generic.ListView):
     ''' Method to movie post corousel'''
     queryset = Movie.objects.filter(status=1)
     template_name = "films/films.html"
     paginate_by = 3
 
-def MovieDetailView(request, slug):
-    ''' to display selected movie post details'''
-    queryset = Movie.objects.filter(status=1)
-    movie = get_object_or_404(queryset, slug=slug)
 
+def NewsLetterView(request):
+    '''to display newsletter form'''
+    if request.method == "POST":
+        newsletter_form = NewsLetterForm(data=request.POST)
+        if newsletter_form.is_valid():
+            newsletter_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'You have succesfully subscribed to our Newsletter'
+            )
+        else:
+            messages.add_message(
+                request, messages.SUCCESS,
+                'You have already subscribed to newsletter with this email.'
+    )
+
+    newsletter_form = NewsLetterForm()
+    
     return render(
-    request,
-    "movies/movie_detail.html",
-    {
-        "movie": movie,
-    },
-)
+        request,
+        "films/newsletter.html",
+        {
+            "newsletter_form": newsletter_form
+        },
+    )
