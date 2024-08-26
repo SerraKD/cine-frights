72e9f1f6f34b0c7bde937b0856c27b053214b713
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-26 08:05:35 +0000

    Create a form field for recommending movie

--- movies/forms.py
+++ movies/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment
+from .models import Comment, RecommendMovie
 from django import forms
 
 
@@ -6,3 +6,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content',)
+
+class RecommendMovieForm(forms.ModelForm):
+    ''' Recommend a movie field'''
+    class Meta:
+        model = RecommendMovie
+        fields = ('username', 'email', 'movie_title', 'additional_comments')
