0ee4e0dd996750eb4896a8d436c17e381220e00f
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-26 18:00:15 +0000

    Register the newsletter model to admin.py

--- films/admin.py
+++ films/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from movies.models import Movie, Comment
+from .models import NewsLetter
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-
+admin.site.register(NewsLetter)
