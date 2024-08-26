f75cded7feaa82a417a6ef2973b89c8e2aac3c41
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-26 17:59:31 +0000

    Create a model for newsletter in models.py

--- films/models.py
+++ films/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class NewsLetter(models.Model):
+    '''Class for newsletter subscription '''
+    email = models.EmailField(unique=True)
+    confirm = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email 
