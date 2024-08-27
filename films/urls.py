8f40a0dd0586e5cde706fd03894ae5e8c2b2a78f
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-27 12:48:26 +0000

    Change the url path view name to membersview in urls.py

--- films/urls.py
+++ films/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.FilmsPageView.as_view(), name='films'),
-    path('newsletter/', views.NewsLetterView, name='news_letter'),
+    path('newsletter/', views.MembersView, name='news_letter'),
 ]
