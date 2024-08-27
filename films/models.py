6b252ee1c41b168177600194e462a7f2bf53b55e
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-27 12:46:14 +0000

    Change the name of subscribtion model to member, add a new model for Newsletter and cloudinary import in models.py

--- films/models.py
+++ films/models.py
@@ -1,9 +1,19 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-class NewsLetter(models.Model):
+class Member(models.Model):
     '''Class for newsletter subscription '''
     email = models.EmailField(unique=True)
     confirm = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email 
+
+class Newsletter(models.Model):
+    ''' Class for newsletter '''
+    subject = models.CharField(max_length=200, unique=True, default="subject")
+    contents = models.TextField(blank=False, null=False, default="text")
+    images = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.subject
