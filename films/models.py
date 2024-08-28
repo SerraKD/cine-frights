4c4038964c81e52c6837e5cc976b4ca966aa8f40
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-28 20:35:45 +0000

    Add a blank lines and remove trailing whitespace in models.py

--- films/models.py
+++ films/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 
+
 class Member(models.Model):
     '''Class for newsletter subscription '''
     email = models.EmailField(unique=True)
     confirm = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.email 
+        return self.email
+
 
 class Newsletter(models.Model):
     ''' Class for newsletter '''
