e4299013593657b4d7f1f6fb4aca3eeda6b0801f
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-29 08:09:53 +0000

    Add reverse, getobject and redirect imports to views.py

--- films/views.py
+++ films/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
 from django.http import HttpResponse
 from django.views import generic
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from movies.models import Movie, Comment
 from .models import Member
 from .forms import MemberForm
