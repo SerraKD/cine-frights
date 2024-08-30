8a8ca752dc0f125b610be65e8aa33f089e2d0b23
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-30 07:56:01 +0000

    Fix the import order and docstrings in forms.py

--- movies/forms.py
+++ movies/forms.py
@@ -1,16 +1,20 @@
-from .models import Comment, RecommendMovie
 from django import forms
+from .models import Comment, RecommendMovie
 
 
 class CommentForm(forms.ModelForm):
-    ''' Movie comment field'''
+    """
+    Movie comment field
+    """
     class Meta:
         model = Comment
         fields = ('content',)
 
 
 class RecommendMovieForm(forms.ModelForm):
-    ''' Recommend a movie field'''
+    """
+    Recommend a movie field
+    """
     class Meta:
         model = RecommendMovie
         fields = ('username', 'email', 'movie_title', 'additional_comments')
