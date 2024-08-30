20394f94c9136cb9769fa4526df3a02801e035a3
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-30 15:27:50 +0000

    Break down long lines in models.py

--- movies/models.py
+++ movies/models.py
@@ -15,18 +15,24 @@ class Movie(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="movie_posts")
     movie_poster = CloudinaryField('image', default='placeholder')
-    release_date = models.SlugField(blank=False, null=False, default=0, max_length=4)
+    release_date = models.SlugField(
+        blank=False, null=False, default=0, max_length=4)
     summary = models.TextField(blank=False, null=False, default="text")
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=POST_STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
     excerpt = models.TextField(blank=True)
-    imdb_rating = models.TextField(blank=False, null=False, max_length=3, default="0")
-    rotten_tomatoes_rating = models.TextField(blank=False, null=False, max_length=3, default="0")
-    our_rating = models.TextField(blank=False, null=False,  max_length=26, default="0")
+    imdb_rating = models.TextField(
+        blank=False, null=False, max_length=3, default="0")
+    rotten_tomatoes_rating = models.TextField(
+        blank=False, null=False, max_length=3, default="0")
+    our_rating = models.TextField(
+        blank=False, null=False,  max_length=26, default="0")
 
     class Meta:
-        '''A meta class for meta data, posts are listed from newest to oldest'''
+        '''
+        A meta class for meta data, posts are listed from newest to oldest
+        '''
         ordering = ["-created_on"]
 
     def __str__(self):
@@ -46,7 +52,9 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        '''A meta class for meta data, comments are listed from newest to oldest'''
+        '''
+        A meta class for meta data, comments are listed from newest to oldest
+        '''
         ordering = ["-created_on"]
 
     def __str__(self):
