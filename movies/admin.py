01e0aa4b958548b1a1aa69b78962f5205e77c9fe
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-30 07:53:49 +0000

    Fix the import order and docstrings in admin.py

--- movies/admin.py
+++ movies/admin.py
@@ -1,20 +1,27 @@
 from django.contrib import admin
-from .models import Movie, Comment, RecommendMovie
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Movie, Comment, RecommendMovie
+
 
 @admin.register(Movie)
 class MoviePostAdmin(SummernoteModelAdmin):
-    '''Registers Movie Posts to admin'''
+    """
+    Registers Movie Posts to admin
+    """
     list_display = ('title', 'slug', 'status')
     search_fields = ['title']
     list_filter = ('status',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('summary',)
 
+
 admin.site.register(Comment)
+
 
 @admin.register(RecommendMovie)
 class RecommendMovieAdmin(admin.ModelAdmin):
-    '''Registers movie recommendations to admin'''
+    """
+    Registers movie recommendations to admin
+    """
 
     list_display = ('movie_title', 'additional_comments', 'read',)
