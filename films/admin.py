2e3b18213772f4d8e4e517728153468351490544
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-30 07:59:01 +0000

    Fix the import order and remove comment in admin.py

--- films/admin.py
+++ films/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from movies.models import Movie, Comment
 from .models import Member, Newsletter
-from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
 admin.site.register(Member)
 admin.site.register(Newsletter)
