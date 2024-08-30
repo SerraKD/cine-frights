e81fe1f05ec0a49a7b054d7f7594271e837cd74a
Serra Kir DWORCZAK <serrakir@gmail.com>
2024-08-30 15:46:37 +0000

    Break down long lines in urls.py

--- movies/urls.py
+++ movies/urls.py
@@ -6,6 +6,14 @@ urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
     path('recommend/', views.recommend_view, name='recommend'),
     path('<slug:slug>/', views.MovieDetailView, name='movie_detail'),
-    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
+    path(
+        '<slug:slug>/edit_comment/<int:comment_id>',
+        views.comment_edit,
+        name='comment_edit'
+    ),
+    path(
+        '<slug:slug>/delete_comment/<int:comment_id>',
+        views.comment_delete,
+        name='comment_delete'
+    ),
 ]
